f08e368051dafdf87eb0f3f7a46d72801451df5b
tutuming <tutuming@gmail.com>
2017-01-22 18:47:57 +0900

    add level parser

--- level.go
+++ level.go
@@ -1,5 +1,10 @@
 package ulog
 
+import (
+	"fmt"
+	"strings"
+)
+
 // Level type
 type Level uint8
 
@@ -14,15 +19,42 @@ const (
 
 func (l Level) String() string {
 	switch l {
-	case ErrorLevel:
-		return "error"
-	case WarnLevel:
-		return "warn"
-	case InfoLevel:
-		return "info"
 	case DebugLevel:
 		return "debug"
+	case InfoLevel:
+		return "info"
+	case WarnLevel:
+		return "warn"
+	case ErrorLevel:
+		return "error"
 	default:
 		return ""
 	}
+}
+
+var levelMap = map[string]Level{
+	"debug":   DebugLevel,
+	"info":    InfoLevel,
+	"warn":    WarnLevel,
+	"warning": WarnLevel,
+	"error":   ErrorLevel,
+}
+
+// ParseLevel parses string to Level
+func ParseLevel(s string) (Level, error) {
+	l, ok := levelMap[strings.ToLower(s)]
+	if !ok {
+		return 0, fmt.Errorf("unknown level %s", s)
+	}
+	return l, nil
+}
+
+// MustLevel parses string to Level.
+// Panics when unknwon level given
+func MustLevel(s string) Level {
+	l, err := ParseLevel(s)
+	if err != nil {
+		panic(err)
+	}
+	return l
 }
